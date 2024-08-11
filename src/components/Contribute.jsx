259ff4221314eda9a273dcc6d8e5899a8da1dc7a
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-11 16:34:33 +0530

    FCC Random Quote Generator

---
+++ src/components/Contribute.jsx
@@ -0,0 +1,13 @@
+import { FaHeart } from "react-icons/fa";
+
+function Contribute() {
+  return (
+    <>
+      <p className="myName">
+        Made by <FaHeart style={{ color: "red" }} /> Yash Junagade
+      </p>
+    </>
+  );
+}
+
+export default Contribute;
